5cddd6e25e6cb804ac015d78c2a1a43370dcc827
wincrane <izg0820@naver.com>
2019-04-10 12:20:37 +0900

    1874

--- 1874.cpp
+++ 1874.cpp
@@ -1,57 +1,54 @@
-#include <iostream>
-#include <stack>
-#include <vector>
-#include <queue>
-
-using namespace std;
-
-int cnt=0;
-queue<int> q;
-void check(stack<int> a);
-void show(stack<int> a);
-
-int main() {
-
-	int t;
-	int temp;
-	int front;
-	stack<int> a;
-	cin >> t;
-	
-	for (int i = 0; i < t; i++) {
-		cin >> temp;
-		q.push(temp);
-	}
-	
-	for (int i = 1; i <= t; i++) {
-		a.push(i);
-		show(a);
-		cout << "+" << endl;
-		check(a);
-		
-	}
-
-
-}
-
-void check(stack<int> a) {
-	
-
-	if (q.front() == a.top()) {
-		q.pop();
-		a.pop();
-		cout << "-" << endl;
-		check(a);
-		
-	}
-
-}
-
-void show(stack<int> a) {
-	for (int i = 0; i < a.size()+1; i++) {
-		cout << a.top() << " ";
-		a.pop();
-	}
-
-}
-
+//#include <iostream>
+//#include <stack>
+//#include <vector>
+//#include <queue>
+//
+//using namespace std;
+//
+//queue<int> q;
+//stack<int> s;
+//vector<bool> r;
+//void check();
+//
+//int main() {
+//
+//	int t;
+//	int temp;
+//	int front;
+//
+//	cin >> t;
+//	
+//	for (int i = 0; i < t; i++) {
+//		cin >> temp;
+//		q.push(temp);
+//	}
+//	
+//	for (int i = 1; i <= t; i++) {
+//		s.push(i);
+//		r.push_back(true);
+//		check();
+//	}
+//
+//	if (!q.empty()) {
+//		cout << "NO\n";
+//	}
+//	else {
+//		for (int i = 0; i < r.size(); i++) {
+//			if (r[i])
+//				cout << "+\n";
+//			else
+//				cout << "-\n";
+//		}
+//	}
+//}
+//
+//void check() {
+//	if (!q.empty() && !s.empty()) {
+//		if (q.front() == s.top()) {
+//			q.pop();
+//			s.pop();
+//			r.push_back(false);
+//			check();
+//		}
+//	}
+//}
