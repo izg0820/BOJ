565d719ce1b23c80de78f6980061db8eaf8bc0ac
wincrane <izg0820@naver.com>
2019-04-14 22:25:30 +0900

    2293

--- 2293.cpp
+++ 2293.cpp
@@ -1,26 +1,33 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	
-	int n;
-	int goal; 
-	int dp[101][10001];
-
-	cin >> n; cin >> goal;
-
-	dp[1][1] = 1;
-	for (int i = 1; i < goal; i++) {
-		if (goal >= n)
-			dp[goal][n] = dp[goal][n - 1] + dp[goal - n][n];
-		else
-			continue;
-	}
-
-	cout << dp[goal][n] << " ";
-
-}
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//
+//	int n;
+//	int k;
+//	int dp[10001] = { 0, };
+//	int temp;
+//	vector<int> coin;
+//
+//	cin >> n >> k;
+//
+//	for (int i = 0; i < n; i++) {
+//		cin >> temp;
+//		coin.push_back(temp);
+//	}
+//
+//	dp[0] = 1;
+//	for (int j = 0; j < n; j++) { //token 수만큼
+//		for (int i = coin[j]; i <= k; i++) {//목표까지 
+//			
+//				dp[i] = dp[i] + dp[i-coin[j]];
+//		}
+//	}
+//
+//	cout << dp[k] << "\n";
+//}
