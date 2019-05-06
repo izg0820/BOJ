d84f9de2a3edf27c5b5c5c89ca2301b4af2d6d7b
wincrane <izg0820@naver.com>
2019-05-06 21:47:58 +0900

    10253.cpp

---
+++ 10253.cpp
@@ -0,0 +1,27 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//
+//	int T;
+//	long long a, b;
+//
+//	cin >> T;
+//	for (int i = 0; i < T; i++) {
+//		cin >> a >> b;
+//		int x;
+//		while (a) {
+//
+//			x = (b - 1) / a + 1;
+//			a = a * x - b;
+//			b = b * x;
+//
+//			
+//		}
+//		cout << x << "\n";
+//	}
+//}
